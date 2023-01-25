4bf33bac2e8dcc89705e1f2ebc9a7288c546eb38
Tonmoy <ulrichtonmoy1@gmail.com>
2023-01-26 01:16:50 +0600

    removed old

--- old/src/components/particles/ParticlesBackground.js
+++
@@ -1,6 +0,0 @@
-import Particles from "react-particles-js";
-import ParticlesConfig from "./particle-config";
-
-export default function ParticlesBackground() {
-    return <Particles params={ParticlesConfig}></Particles>;
-}
