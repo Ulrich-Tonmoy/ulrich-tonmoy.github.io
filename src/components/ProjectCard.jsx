0f00f560abbb8f65e2b2d66c38654c8c99dd14fe
Tonmoy <ulrichtonmoy1@gmail.com>
2023-02-08 01:52:13 +0600

    final update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,41 +1,86 @@
 import styles from "../styles/ProjectCard.module.css";
+import { motion } from "framer-motion";
+import { urlFor } from "./../client";
+import { EyeIcon, GithubIcon } from "./icons";
+import { useState } from "react";
 
 const ProjectCard = ({ project }) => {
+    const [showModal, setShowModal] = useState(false);
+    const getColor = () => {
+        return Math.floor(Math.random() * 16777215).toString(16);
+    };
+
     return (
-        <div className={styles.card}>
-            <img src={project.image} height={300} width={600} alt={project.name} />
-            <div className={styles.content}>
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
-                <div className={styles.tags}>
-                    {project.tags.map((tag) => (
-                        <span key={tag} className={tag}>
-                            {tag}
-                        </span>
-                    ))}
-                </div>
-                <div className={styles.cta}>
-                    {project.source_code && (
-                        <a
-                            href={project.source_code}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className={styles.underline}
-                        >
-                            Source Code
+        <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className={`${styles.projectItem} ${styles.flex}`}
+        >
+            <div className={`${styles.img} ${styles.flex}`}>
+                <img src={urlFor(project?.imgUrl)} alt={project?.name} />
+                <motion.div
+                    whileHover={{ opacity: [0, 1] }}
+                    transition={{
+                        duration: 0.25,
+                        ease: "easeInOut",
+                        staggerChildren: 0.5,
+                    }}
+                    className={` ${styles.projectHover} ${styles.flex}`}
+                >
+                    {project?.projectLink && (
+                        <a href={project?.projectLink} target="_blank" rel="noreferrer">
+                            <motion.div
+                                whileInView={{ scale: [0, 1] }}
+                                whileHover={{ scale: [1, 0.9] }}
+                                transition={{ duration: 0.25 }}
+                                className={styles.flex}
+                            >
+                                <EyeIcon />
+                            </motion.div>
                         </a>
                     )}
-                    <a
-                        href={project.demo}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.underline}
-                    >
-                        Live Demo
-                    </a>
+                    {project?.codeLink && (
+                        <a href={project?.codeLink} target="_blank" rel="noreferrer">
+                            <motion.div
+                                whileInView={{ scale: [0, 1] }}
+                                whileHover={{ scale: [1, 0.9] }}
+                                transition={{ duration: 0.25 }}
+                                className={styles.flex}
+                            >
+                                <GithubIcon />
+                            </motion.div>
+                        </a>
+                    )}
+                </motion.div>
+            </div>
+            <div className={`${styles.content} ${styles.flex}`}>
+                <h4 className={styles.boldText}>{project?.title}</h4>
+                <div className={styles.techContainer}>
+                    {showModal &&
+                        project?.techs?.map((tech, index) => (
+                            <div
+                                className={`${styles.tech}`}
+                                style={{ border: `0.1rem solid #${getColor()}` }}
+                                key={index}
+                            >
+                                <p className={styles.text}>{tech}</p>
+                            </div>
+                        ))}
+                </div>
+                <p className={styles.text} style={{ marginTop: 10 }}>
+                    {showModal
+                        ? project?.description
+                        : `${project?.description.substring(0, 70)}...`}
+                    <button onClick={() => setShowModal(!showModal)}>
+                        {showModal ? "hide" : "show"}
+                    </button>
+                </p>
+                <div className={`${styles.tag} ${styles.flex}`}>
+                    <p className={styles.text}>{project?.tags[0]}</p>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,8 +2,7 @@ import ProjectCard from "../components/ProjectCard";
 import styles from "../styles/ProjectsPage.module.css";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { client, urlFor } from "./../client";
-import { EyeIcon, GithubIcon } from "../components/icons";
+import { client } from "./../client";
 
 const Projects = () => {
     const [activeFilter, setActiveFilter] = useState("All");
@@ -64,60 +63,7 @@ const Projects = () => {
                 className={styles.card}
             >
                 {filterProject?.map((project, index) => (
-                    <motion.div
-                        whileInView={{ opacity: 1 }}
-                        whileHover={{ scale: 1.1 }}
-                        transition={{ duration: 0.5, type: "tween" }}
-                        className={`${styles.projectItem} ${styles.flex}`}
-                        key={index}
-                    >
-                        <div className={`${styles.img} ${styles.flex}`}>
-                            <img src={urlFor(project?.imgUrl)} alt={project?.name} />
-                            <motion.div
-                                whileHover={{ opacity: [0, 1] }}
-                                transition={{
-                                    duration: 0.25,
-                                    ease: "easeInOut",
-                                    staggerChildren: 0.5,
-                                }}
-                                className={` ${styles.projectHover} ${styles.flex}`}
-                            >
-                                {project?.projectLink && (
-                                    <a href={project?.projectLink} target="_blank" rel="noreferrer">
-                                        <motion.div
-                                            whileInView={{ scale: [0, 1] }}
-                                            whileHover={{ scale: [1, 0.9] }}
-                                            transition={{ duration: 0.25 }}
-                                            className={styles.flex}
-                                        >
-                                            <EyeIcon />
-                                        </motion.div>
-                                    </a>
-                                )}
-                                {project?.codeLink && (
-                                    <a href={project?.codeLink} target="_blank" rel="noreferrer">
-                                        <motion.div
-                                            whileInView={{ scale: [0, 1] }}
-                                            whileHover={{ scale: [1, 0.9] }}
-                                            transition={{ duration: 0.25 }}
-                                            className={styles.flex}
-                                        >
-                                            <GithubIcon />
-                                        </motion.div>
-                                    </a>
-                                )}
-                            </motion.div>
-                        </div>
-                        <div className={`${styles.content} ${styles.flex}`}>
-                            <h4 className={styles.boldText}>{project?.title}</h4>
-                            <p className={styles.text} style={{ marginTop: 10 }}>
-                                {project?.description.substring(0, 90)}...
-                            </p>
-                            <div className={`${styles.tag} ${styles.flex}`}>
-                                <p className={styles.text}>{project?.tags[0]}</p>
-                            </div>
-                        </div>
-                    </motion.div>
+                    <ProjectCard project={project} key={index} />
                 ))}
             </motion.div>
         </motion.div>
