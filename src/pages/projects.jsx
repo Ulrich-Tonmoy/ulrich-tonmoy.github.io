f9ee3ad862ac874639954de44fa6d2b38b3b2dde
Tonmoy <ulrichtonmoy1@gmail.com>
2023-05-31 22:42:59 +0600

    feat: made project and skill filter dynamic with sanity

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,68 +5,73 @@ import { motion } from "framer-motion";
 import { client } from "./../client";
 
 const Projects = () => {
-    const [activeFilter, setActiveFilter] = useState("All");
-    const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-    const [projects, setProjects] = useState([]);
-    const [filterProject, setFilterProject] = useState([]);
+  const [filters, setFilters] = useState(["All"]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [projects, setProjects] = useState([]);
+  const [filterProject, setFilterProject] = useState([]);
 
-    useEffect(() => {
-        const query = '*[_type == "projects"]';
-        client.fetch(query).then((res) => {
-            setProjects(res);
-            setFilterProject(res);
-        });
-    }, []);
+  useEffect(() => {
+    const query = '*[_type == "projects"]';
+    const projectsFilterQuery = '*[_type == "filters"]';
+    client.fetch(projectsFilterQuery).then((res) => {
+      setFilters(res.filter((x) => x.name === "Project")[0].filter);
+    });
+    client.fetch(query).then((res) => {
+      setProjects(res);
+      setFilterProject(res);
+    });
+  }, []);
 
-    const handleWorkFilter = (item) => {
-        setActiveFilter(item);
-        setAnimateCard([{ y: 100, opacity: 0 }]);
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
-        setTimeout(() => {
-            setAnimateCard([{ y: 0, opacity: 1 }]);
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-            if (item === "All") {
-                setFilterProject(projects);
-            } else {
-                setFilterProject(projects.filter((project) => project.tags.includes(item)));
-            }
-        }, 500);
-    };
+      if (item === "All") {
+        setFilterProject(projects);
+      } else {
+        setFilterProject(projects.filter((project) => project.tags.includes(item)));
+      }
+    }, 500);
+  };
 
-    return (
-        <motion.div
-            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-            transition={{ duration: 0.5 }}
-            className={styles.container}
-        >
-            <h2 className={styles.headText}>
-                I Like<span> Making Stuff</span>
-                <br />
-                and <span> Putting it on the Internet</span>
-            </h2>
-            <div className={styles.filter}>
-                {["Web", "MERN", "Mobile", "Desktop", "Game", "AI/ML", "All"].map((item, index) => (
-                    <div
-                        key={index}
-                        onClick={() => handleWorkFilter(item)}
-                        className={`${styles.filterItem} ${styles.flex} ${styles.text} ${
-                            activeFilter === item ? `${styles.active}` : ""
-                        }`}
-                    >
-                        {item}
-                    </div>
-                ))}
-            </div>
-            <motion.div
-                animate={animateCard}
-                transition={{ duration: 0.5, delayChildren: 0.5 }}
-                className={styles.card}
-            >
-                {filterProject?.map((project, index) => (
-                    <ProjectCard project={project} key={index} />
-                ))}
-            </motion.div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+      transition={{ duration: 0.5 }}
+      className={styles.container}
+    >
+      <h2 className={styles.headText}>
+        I Like<span> Making Stuff</span>
+        <br />
+        and <span> Putting it on the Internet</span>
+      </h2>
+      <div className={styles.filter}>
+        {filters?.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`${styles.filterItem} ${styles.flex} ${styles.text} ${
+              activeFilter === item ? `${styles.active}` : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className={styles.card}
+      >
+        {filterProject?.map((project, index) => (
+          <ProjectCard project={project} key={index} />
+        ))}
+      </motion.div>
+    </motion.div>
+  );
 };
 export default Projects;
