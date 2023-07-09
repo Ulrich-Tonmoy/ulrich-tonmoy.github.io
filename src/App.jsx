a0a03c3d92f9397d8042b71a7b5326de0d89d8ee
Tonmoy <ulrichtonmoy1@gmail.com>
2023-07-10 01:06:12 +0600

    feat: 3d desk home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, Skills, Contact, Github, Home, Projects, Settings, NotFound } from "./pages";
 
 function App() {
-    useEffect(() => {
-        if (localStorage.getItem("theme")) {
-            document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
-        }
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem("theme")) {
+      document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
+    }
+  }, []);
 
-    return (
-        <Router>
-            <Layout>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/home" element={<Home />} />
-                    <Route exact path="/about" element={<About />} />
-                    <Route exact path="/skills" element={<Skills />} />
-                    <Route exact path="/projects" element={<Projects />} />
-                    <Route exact path="/github" element={<Github />} />
-                    <Route exact path="/contact" element={<Contact />} />
-                    <Route exact path="/settings" element={<Settings />} />
-                    <Route path="/*" element={<NotFound />} />
-                </Routes>
-            </Layout>
-        </Router>
-    );
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/skills" element={<Skills />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/github" element={<Github />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/settings" element={<Settings />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
