14f749b37979a6f6e0b4c51fefb36a89ba699d74
Ulrich <ulrichtonmoy1@gmail.com>
2023-12-05 18:13:09 +0600

    🎨style: navigation bar added

--- src/app/portfolio/3d/layout.tsx
+++ src/app/portfolio/3d/layout.tsx
@@ -1,5 +1,6 @@
 import { Work_Sans } from "next/font/google";
 import { Metadata } from "next";
+import Navbar from "@/components/navbar";
 
 const WorkSans = Work_Sans({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 const Portfolio3DLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className={`${WorkSans.className} bg-slate-300/20`}>
+      <Navbar />
       {children}
     </main>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const path = usePathname().split("3d/")[1];
+
+  return (
+    <header className="header">
+      <Link
+        href="/portfolio/3d"
+        className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">Tonmoy</p>
+      </Link>
+      <nav className="flex text-lg gap-7 font-medium">
+        <Link
+          href="/portfolio/3d/about"
+          className={`${path === "about" ? "text-blue-500" : "text-black"}`}
+        >
+          About
+        </Link>
+        <Link
+          href="/portfolio/3d/projects"
+          className={`${path === "projects" ? "text-blue-500" : "text-black"}`}
+        >
+          Projects
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
