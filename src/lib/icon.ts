9919abb8255e172db2448353e7314e5f67da5751
Ulrich <ulrichtonmoy1@gmail.com>
2023-12-05 20:01:02 +0600

    🧹chore: icons import file moved to lib

---
+++ src/lib/icon.ts
@@ -0,0 +1,59 @@
+import css from "../../public/icons/css.svg";
+import express from "../../public/icons/express.svg";
+import git from "../../public/icons/git.svg";
+import github from "../../public/icons/github.svg";
+import html from "../../public/icons/html.svg";
+import javascript from "../../public/icons/javascript.svg";
+import mongodb from "../../public/icons/mongodb.svg";
+import motion from "../../public/icons/motion.svg";
+import mui from "../../public/icons/mui.svg";
+import nextjs from "../../public/icons/nextjs.svg";
+import nodejs from "../../public/icons/nodejs.svg";
+import react from "../../public/icons/react.svg";
+import redux from "../../public/icons/redux.svg";
+import sass from "../../public/icons/sass.svg";
+import tailwindcss from "../../public/icons/tailwindcss.svg";
+import typescript from "../../public/icons/typescript.svg";
+import linkedin from "../../public/icons/linkedin.svg";
+import twitter from "../../public/icons/twitter.svg";
+import car from "../../public/icons/car.svg";
+import estate from "../../public/icons/estate.svg";
+import pricewise from "../../public/icons/pricewise.svg";
+import snapgram from "../../public/icons/snapgram.svg";
+import summiz from "../../public/icons/summiz.svg";
+import threads from "../../public/icons/threads.svg";
+import arrow from "../../public/icons/arrow.svg";
+import contact from "../../public/icons/contact.svg";
+import soundon from "../../public/icons/soundon.png";
+import soundoff from "../../public/icons/soundoff.png";
+
+export {
+  css,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  linkedin,
+  twitter,
+  car,
+  estate,
+  pricewise,
+  snapgram,
+  summiz,
+  threads,
+  arrow,
+  contact,
+  soundon,
+  soundoff,
+};
