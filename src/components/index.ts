8029de0a8f65ec8c7678c5b790904dc51aa11f2e
Ulrich <ulrichtonmoy1@gmail.com>
2023-12-06 19:14:23 +0600

    🧹chore: file clean up

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,7 @@
 "use client";
 
-export { default as About } from "./about";
 export { default as Alert } from "./alert";
-export { default as Contact } from "./contact";
 export { default as CTA } from "./cta";
-export { default as Home } from "./home";
 export { default as HomeInfo } from "./home-info";
 export { default as Loader } from "./loader";
 export { default as Navbar } from "./navbar";
