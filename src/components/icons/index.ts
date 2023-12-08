ab0956e7ef923713e9ae77a4219718f7a1b86ab7
Ulrich <ulrichtonmoy1@gmail.com>
2023-12-08 16:36:40 +0600

    💄style: icons component created

---
+++ src/components/icons/index.ts
@@ -0,0 +1,16 @@
+export { default as AccountIcon } from "./account-icon";
+export { default as BellIcon } from "./bell-icon";
+export { default as CheckIcon } from "./check-icon";
+export { default as ChevronRight } from "./chevron-right";
+export { default as CodeIcon } from "./code-icon";
+export { default as ErrorIcon } from "./error-icon";
+export { default as FilesIcon } from "./files-icon";
+export { default as GithubIcon } from "./github-icon";
+export { default as EyeIcon } from "./eye-icon";
+export { default as MailIcon } from "./mail-icon";
+export { default as PencilIcon } from "./pencil-icon";
+export { default as ReactIcon } from "./react-icon";
+export { default as SettingsIcon } from "./settings-icon";
+export { default as SourceControlIcon } from "./source-control-icon";
+export { default as WarningIcon } from "./warning-icon";
+export { default as PersonIcon } from "./person-icon";
