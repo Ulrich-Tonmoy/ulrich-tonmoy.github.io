0afc1c403ffdaca32acde0da4a7aab4ae5214e5d
Ulrich <ulrichtonmoy1@gmail.com>
2024-03-26 04:42:03 +0600

    deskTool to structureTool

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,5 +1,5 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: 'jpwqlm1c',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
