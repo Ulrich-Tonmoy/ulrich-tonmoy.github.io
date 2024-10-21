c214d1997e48a6a836a873e3d59dad9a5cea2639
Tonmoy <ulrichtonmoy1@gmail.com>
2024-10-22 03:52:08 +0600

    💄add close icon in tabs

--- src/components/vs-code/tab.tsx
+++ src/components/vs-code/tab.tsx
@@ -2,16 +2,25 @@ import Link from "next/link";
 import styles from "@/styles/vs-code/tab.module.css";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { CloseIcon } from "./icons";
+import { cn } from "@/lib/utils";
 
 const Tab = ({ icon, filename, path }: any) => {
   const pathname = usePathname();
 
   return (
-    <Link href={path}>
-      <div className={`${styles.tab} ${pathname === path && styles.active}`}>
-        <Image src={icon} alt={filename} height={18} width={18} />
-        <p>{filename}</p>
-      </div>
+    <Link
+      href={path}
+      className={cn(
+        `${styles.tab} ${pathname === path && styles.active}`,
+        "group"
+      )}
+    >
+      <Image src={icon} alt={filename} height={18} width={18} />
+      <p>{filename}</p>
+      <span className="ml-2 p-1 rounded-md flex items-center group-hover:visible invisible hover:bg-white/20">
+        <CloseIcon />
+      </span>
     </Link>
   );
 };
